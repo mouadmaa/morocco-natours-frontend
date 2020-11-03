7f187acd27b9f86c0b9afd19c36313182b8a90be
mouadmaa <maaroufimouad00@gmail.com>
2020-11-03 01:32:44 +0100

    restyle not found page

---
+++ src/Layout/NotFound/NotFound.styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const NotFoundContainer = styled.div`
+  max-height: 90vh;
+  height: 100%;
+  padding: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  > * {
+    margin-bottom: 2rem;
+  }
+`
+
+export const NotFoundImage = styled.img`
+  height: 100%;
+`
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,36 @@
 import React, { FC } from 'react'
-import { PageProps } from 'gatsby'
+import { graphql, PageProps } from 'gatsby'
 
-import Heading from '../components/UI/Heading/Heading.component'
+import {
+  NotFoundContainer, NotFoundImage
+} from '../Layout/NotFound/NotFound.styles'
+import Button from '../components/UI/Button/Button.component'
 
-const NotFound: FC<PageProps> = () => {
+const NotFound: FC<PageProps> = props => {
+  const { data } = props as any
+
   return (
-    <Heading type='Secondary'>
-      Not Found 404
-    </Heading>
+    <NotFoundContainer>
+      <NotFoundImage
+        src={data.notFoundSvg.publicURL}
+        alt='not found'
+      />
+      <Button
+        type='buttonLink'
+        to='/'
+      >
+        Go Back To Home
+      </Button>
+    </NotFoundContainer>
   )
 }
 
 export default NotFound
+
+export const query = graphql`
+  {
+    notFoundSvg: file(relativePath: {eq: "not-found.svg"}) {
+      publicURL
+    }
+  }
+`
