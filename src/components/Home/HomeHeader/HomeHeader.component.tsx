bbf602592ddcba07926e8e9c884eb2a5b1b14137
mouadmaa <maaroufimouad00@gmail.com>
2020-11-19 19:39:09 +0100

    implement animation to home header with framer motion

--- src/components/Home/HomeHeader/HomeHeader.component.tsx
+++ src/components/Home/HomeHeader/HomeHeader.component.tsx
@@ -1,78 +1,120 @@
-import React, { FC } from 'react'
+import React, { FC, useLayoutEffect, useRef, useState } from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
+import { motion, useTransform, useViewportScroll } from 'framer-motion'
 
 import { HeaderContainer, HeaderTextContainer } from './HomeHeader.styles'
 import Heading from '../../UI/Heading/Heading.component'
 import Button from '../../UI/Button/Button.component'
 
 const HomeHeader: FC = () => {
-  const data = useStaticQuery(query)
+	const data = useStaticQuery(query)
+	const headerRef = useRef<HTMLHeadElement>(null)
+	const [ elementTop, setElementTop ] = useState(0)
 
-  return (
-    <HeaderContainer>
-      <HeaderTextContainer>
-        <Heading>
-          <div>Outdoors</div>
-          <div>is where life happens</div>
-        </Heading>
+	const { scrollY, scrollYProgress } = useViewportScroll()
+	const scale = useTransform(scrollYProgress, [ 0, 1 ], [ 1, -3 ])
+	const y = useTransform(scrollY, [ elementTop, elementTop - 6 ], [ 1, -1 ], { clamp: false })
 
-        <Button
-          color='white'
-          type='buttonLink'
-          to='/overview'
-        >
-          Discover our tours
-        </Button>
-      </HeaderTextContainer>
+	useLayoutEffect(
+		() => {
+			if (headerRef.current) setElementTop(headerRef.current.offsetTop)
+		},
+		[ headerRef ],
+	)
 
-      <Image fluid={data.personImage.childImageSharp.fluid} alt="person" />
-      <Image fluid={data.mountain1Image.childImageSharp.fluid} alt="mountain 1" />
-      <Image fluid={data.mountain2Image.childImageSharp.fluid} alt="mountain 2" />
-      <Image fluid={data.mountain3Image.childImageSharp.fluid} alt="mountain 3" />
-      <Image fluid={data.skyImage.childImageSharp.fluid} alt="sky" />
-    </HeaderContainer>
-  )
+	const images = [
+		{ numberTop: 18, fluid: data.person.childImageSharp.fluid, alt: 'person' },
+		{ numberTop: 20, fluid: data.mountain1.childImageSharp.fluid, alt: 'mountain 1' },
+		{ numberTop: -5, fluid: data.mountain2.childImageSharp.fluid, alt: 'mountain 2' },
+		{ numberTop: -6, fluid: data.mountain3.childImageSharp.fluid, alt: 'mountain 3' },
+		{ numberTop: -4, fluid: data.sky.childImageSharp.fluid, alt: 'sky' },
+	]
+
+	return (
+		<HeaderContainer ref={headerRef}>
+			<HeaderTextContainer style={{ opacity: scale, y }}>
+				<Heading>
+					<div>Outdoors</div>
+					<div>is where life happens</div>
+				</Heading>
+
+				<Button color='white' type='buttonLink' to='/overview'>
+					Discover our tours
+				</Button>
+			</HeaderTextContainer>
+
+			{images.map((image) => (
+				<HomeHeaderImage
+					key={image.alt}
+					elementTop={elementTop}
+					numberTop={image.numberTop}
+					fluid={image.fluid}
+					alt={image.alt}
+				/>
+			))}
+		</HeaderContainer>
+	)
 }
 
 export default HomeHeader
 
+interface HomeHeaderImageProps {
+	elementTop: number
+	numberTop: number
+	fluid: any
+	alt: string
+}
+
+const HomeHeaderImage: FC<HomeHeaderImageProps> = (props) => {
+	const { elementTop, numberTop, fluid, alt } = props
+
+	const { scrollY } = useViewportScroll()
+	const y = useTransform(scrollY, [ elementTop, elementTop + numberTop ], [ 1, -1 ], { clamp: false })
+
+	return (
+		<motion.div style={{ y }}>
+			<Image fluid={fluid} alt={alt} />
+		</motion.div>
+	)
+}
+
 const query = graphql`
-  {
-    personImage: file(relativePath: {eq: "person-3.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    mountain1Image: file(relativePath: {eq: "mountain-1.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    mountain2Image: file(relativePath: {eq: "mountain-2.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    mountain3Image: file(relativePath: {eq: "mountain-3.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    skyImage: file(relativePath: {eq: "sky.png"}) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
+	{
+		person: file(relativePath: { eq: "person-3.png" }) {
+			childImageSharp {
+				fluid {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+		mountain1: file(relativePath: { eq: "mountain-1.png" }) {
+			childImageSharp {
+				fluid {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+		mountain2: file(relativePath: { eq: "mountain-2.png" }) {
+			childImageSharp {
+				fluid {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+		mountain3: file(relativePath: { eq: "mountain-3.png" }) {
+			childImageSharp {
+				fluid {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+		sky: file(relativePath: { eq: "sky.png" }) {
+			childImageSharp {
+				fluid {
+					...GatsbyImageSharpFluid
+				}
+			}
+		}
+	}
 `
--- src/components/Home/HomeHeader/HomeHeader.styles.tsx
+++ src/components/Home/HomeHeader/HomeHeader.styles.tsx
@@ -1,110 +1,114 @@
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 export const HeaderContainer = styled.header`
-  width: 100%;
-  height: 100vh;
-  position: relative;
-  overflow: hidden;
+	width: 100%;
+	height: 100vh;
+	position: relative;
+	overflow: hidden;
 
-  :after{
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    top: 0;
-    background-image: linear-gradient(315deg, transparent,  rgba(0, 0, 0, 0.1));
-    /* background-image: linear-gradient(315deg, rgba(8, 174, 234, 0.6), rgba(42, 245, 152, 0.6)); */
-    z-index: 25;
-  }
+	:after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		left: 0;
+		top: 0;
+		background-image: linear-gradient(315deg, transparent, rgba(0, 0, 0, 0.1));
+		z-index: 25;
+	}
 
-  > div:nth-of-type(2) {
-    position: absolute !important;
-    width: 650px;
-    bottom: -100px;
-    left: -70px;
-    z-index: 20;
+	> div:nth-of-type(2) {
+		position: absolute !important;
+		width: 650px;
+		bottom: -100px;
+		left: -70px;
+		z-index: 20;
 
-    @media only screen and (min-width: 90em) {
-      width: 65rem;
-    }
+		@media only screen and (min-width: 90em) {
+			width: 65rem;
+		}
 
-    @media only screen and (max-width: 37.5em) {
-      left: -60%;
-    }
-  }
+		@media only screen and (max-width: 37.5em) {
+			left: -60%;
+		}
+	}
 
-  > div:nth-of-type(3) {
-    position: absolute !important;
-    bottom: -100px;
-    width: 1500px;
-    right: 0;
-    z-index: 19;
+	> div:nth-of-type(3) {
+		position: absolute !important;
+		bottom: -100px;
+		width: 1500px;
+		right: 0;
+		z-index: 19;
 
-    @media only screen and (min-width: 90em) {
-      width: 100%;
-    }
-  }
+		@media only screen and (min-width: 90em) {
+			width: 100%;
+		}
+	}
 
-  > div:nth-of-type(4) {
-    position: absolute !important;
-    width: 1100px;
-    bottom: -100px;
-    left: 0;
-    z-index: 18;
+	> div:nth-of-type(4) {
+		position: absolute !important;
+		width: 1100px;
+		bottom: -100px;
+		left: 0;
+		z-index: 18;
 
-    @media only screen and (min-width: 90em) {
-      width: 80%;
-    }
-  }
+		@media only screen and (min-width: 90em) {
+			width: 80%;
+		}
+	}
 
-  > div:nth-of-type(5) {
-    position: absolute !important;
-    width: 900px;
-    bottom: 150px;
-    right: 0;
-    z-index: 17;
+	> div:nth-of-type(5) {
+		position: absolute !important;
+		width: 900px;
+		bottom: 150px;
+		right: 0;
+		z-index: 17;
 
-    @media only screen and (min-width: 90em) {
-      width: 70%;
-    }
-  }
+		@media only screen and (min-width: 90em) {
+			width: 70%;
+		}
+	}
 
-  > div:nth-of-type(6) {
-    position: absolute !important;
-    width: 2100px;
-    bottom: 250px;
-    right: 0;
+	> div:nth-of-type(6) {
+		position: absolute !important;
+		width: 2100px;
+		bottom: 250px;
+		right: 0;
 
-    @media only screen and (min-width: 100em) {
-      width: 100%;
-      height: auto !important;
-    }
+		@media only screen and (min-width: 100em) {
+			width: 100%;
+			height: auto !important;
+		}
 
-    @media only screen and (min-width: 80em) {
-      height: 80%;
-      top: 0;
-      bottom: 0;
-    }
-  }
+		@media only screen and (min-width: 80em) {
+			height: 80%;
+			top: 0;
+			bottom: 0;
+		}
+	}
 `
 
-export const HeaderTextContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  z-index: 30;
+export const HeaderTextContainer = styled(motion.div)`
+	width: 100%;
+	height: 90%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	z-index: 30;
 
-  a {
-    opacity: 0.9;
-    color:  rgba(0, 0, 0, 0.6);
-    padding: 0.8rem 2.5rem;
-    font-size: 1.4rem;
-  }
+	a {
+		background-color: ${(props) => props.theme.color.grayLight3};
+		color: rgba(0, 0, 0, 0.6);
+		padding: 0.8rem 2.5rem;
+		font-size: 1.4rem;
+
+    :hover {
+      background-color: ${(props) => props.theme.color.grayLight3};
+    }
+	}
 `
